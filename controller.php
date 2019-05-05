fdc4709f21eb096635f78ab8408ac3a3ab5ab864
Anant <anant@anant>
2019-05-05 12:18:25 +0530

    documentation

--- controller.php
+++ controller.php
@@ -2,25 +2,28 @@
 
 require_once('model.php');
 
+// TODO Class controls/executes all todo app operations 
+
 class TODO {
 
-    private $db = null,$ch_id = null,$u_id = null,$command = null, $message = "", $model = 'todo';
+    private $db = null,$ch_id = null,$u_id = null,$command = null, $message = "", $model = 'todo'; // since there is only one schema 'todo'
 
+    // Initialize instance of DB class and create db connection, parse input data (with validations) 
     public function __construct($data){
         $this->db = new DB();
         if($this->db->getConnection() == false){
             $this->handleErrors('Unfortunately, TODO app has stopped working.');
         }
         $this->data = $data;
-        $this->parseData();
+        $this->parseData(); // parse input data with Error handling
     }
 
-    // Executes function respective to command received
+    // Executes function with respect to slack command
     public function executes(){
        return $this->{substr($this->command,1)}();
     }
 
-    // List tasks
+    // get list of all TODOs in a channel
     public function listtodos(){
             
         $whereArr = array('ch_id' => $this->ch_id);
@@ -36,7 +39,7 @@ class TODO {
         }
     }
 
-    // add tasks
+    // Add a TODO task to a channel
     public function addtodo(){
        
         $currentDate = date('d-m-Y H:i:s');
@@ -49,7 +52,7 @@ class TODO {
         }
     }
 
-    // mark todo tasks
+    // Remove a TODO task from a channel
     public function marktodo(){
         $this->message = str_replace('"',"",$this->message);
         $whereArr = array('task' => $this->message, 'ch_id' => $this->ch_id);
@@ -57,16 +60,20 @@ class TODO {
         if($result){
             return $this->sendResponse('Removed TODO for "'.$this->message.'"');
         }else{
-            $this->handleErrors('TODO "'.$this->message.'" does not exist.');
+            $this->handleErrors('TODO task "'.$this->message.'" does not exist.');
         }
     }
 
+    // Generate appropriate response JSON for a slack server
+    // "in_channel" means response text will be visible to every member in a channel
     public function sendResponse($msg){
         $responseArr = array('response_type' => 'in_channel', 'text' => $msg, 'attachments' => array('text'=> ''));
         $responseJson = json_encode($responseArr);
         return $responseJson;    
     }
     
+    // Parse input data and check if empty 
+    // Perform error handling for invalid data
     public function parseData(){
 
         if(!empty($this->data)){
@@ -80,10 +87,11 @@ class TODO {
         }
     }    
 
+    // check if data is valid 
     public function checkParams(){
         
         if(empty($this->message) && ($this->command != "/listtodos") ) {
-            $this->handleErrors('Please mention TODO task.');
+            $this->handleErrors('Please mention the TODO task.');
         }else{
             if(!in_array($this->command, array('/listtodos','/addtodo','/marktodo'))){
                 $this->handleErrors('Invalid slash command.');
@@ -91,7 +99,7 @@ class TODO {
         }
     }
 
-    // Handle all errors
+    // Handle all errors and send appropriate error message to the slack server
     public function handleErrors($error_msg){
         echo $this->sendResponse($error_msg);
         die;
